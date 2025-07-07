d52751da8b2524945f4e54fab2e0f62b7f6b9b38
Ryan Du <ryandu343@gmail.com>
2025-07-07 11:08:24 -0400

    Feat: Added docstrings

--- src/ceblpy/helpers.py
+++ src/ceblpy/helpers.py
@@ -2,6 +2,24 @@ import pandas as pd
 from datetime import datetime
 
 def validate_seasons(seasons):
+    """
+    Checks whether all the provided seasons are valid years and raises an error if not.
+
+    Parameters
+    ----------
+    seasons: list of int
+        A list of years representing seasons to validate.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> validate_seasons([2019, 2020, 2021])
+    >>> validate_seasons([2018, "2020"])  # Raises TypeError
+    >>> validate_seasons([2022, 2023, 2030])  # Raises ValueError
+    """
     if not isinstance(seasons, list):
         raise TypeError("Expected a list of years")
     for year in seasons:
